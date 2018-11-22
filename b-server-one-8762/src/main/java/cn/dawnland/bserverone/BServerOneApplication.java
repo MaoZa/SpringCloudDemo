8c91630ea495bdfc1731f15e118c510352763c94
Cap_Sub <cap_sub@dawnland.cn>
2018-11-22 16:09:15 +0800

    添加新建的文件

---
+++ b-server-one-8762/src/main/java/cn/dawnland/bserverone/BServerOneApplication.java
@@ -0,0 +1,12 @@
+package cn.dawnland.bserverone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BServerOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BServerOneApplication.class, args);
+    }
+}
---
+++ b-server-two-8763/src/main/java/cn/dawnland/bservertwo8763/BServerTwo8763Application.java
@@ -0,0 +1,12 @@
+package cn.dawnland.bservertwo8763;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BServerTwo8763Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BServerTwo8763Application.class, args);
+    }
+}
